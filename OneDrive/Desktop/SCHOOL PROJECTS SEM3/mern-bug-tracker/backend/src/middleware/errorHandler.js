b2162f89c983f0ee7c27b85ee5f7a5db016f9be8
Chelsea Nyariki <nyarikichelsea@gmail.com>
2025-11-11 13:34:22 +0300

    Move server.js to backend root and cleanup src folder for deployment

--- OneDrive/Desktop/SCHOOL PROJECTS SEM3/mern-bug-tracker/backend/src/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(err, req, res, next) {
-  console.error('[ERROR HANDLER]', err && err.message ? err.message : err);
-  res.status(500).json({ error: 'Internal Server Error' });
-}
-
-module.exports = { errorHandler };
