c8d972d542c187ffae178fba42e2d920201976d2
Chelsea Nyariki <nyarikichelsea@gmail.com>
2025-11-12 18:37:42 +0300

    Fix start script for Render

--- OneDrive/Desktop/SCHOOL PROJECTS SEM3/mern-bug-tracker/backend/server.js
+++ OneDrive/Desktop/SCHOOL PROJECTS SEM3/mern-bug-tracker/backend/server.js
@@ -19,17 +19,15 @@ app.get("/", (req, res) => {
   res.send("🚀 Bug Tracker API is running!");
 });
 
-// Example API route
-app.get("/api/bugs", (req, res) => {
-  res.json({ message: "Bug routes work!" });
-});
-
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
+  
+app.get('/health', (req, res) => {
+  res.send('Server OK');
+});
 // Use Render's dynamic port or fallback to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
